865fdc15b00c64b6ecc455abac0eb7a0474f4d7e
Stephen <stephen@agilestyle.com>
2015-01-21 16:48:28 -0700

    Generate unique token for invitation and check for it across all public Invite methods

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -6,24 +6,41 @@ class InvitesController < ApplicationController
 
   def show
     @invite = Invite.find(params[:id])
+    @token = params[:token]
+
+    if @invite.token != @token
+      flash[:error] = 'There is something wrong with your RSVP URL.'
+      redirect_to root_path
+    end
   end
 
   def edit
     @invite = Invite.find(params[:id])
+    @token = params[:token]
+
+    if @invite.token != @token
+      flash[:error] = 'There is something wrong with your RSVP URL.'
+      redirect_to root_path
+    end
   end
 
   def update
     @invite = Invite.find(params[:id])
+    @token = params[:token]
 
-    if @invite.update(invite_params)
+    if @invite.update(invite_params) && @invite.token == @token
       flash[:success] = 'Details Saved'
-      redirect_to :action => 'show'
+      redirect_to :action => 'show', :token => @invite.token
+    else
+      flash[:error] = 'There is something wrong with your RSVP URL.'
+      redirect_to root_path
     end
   end
 
   private
-    def invite_params
-      params.require(:invite).permit(:name, :email, :phone, :num_guests, :status)
-    end
+
+  def invite_params
+    params.require(:invite).permit(:name, :email, :phone, :num_guests, :status, :token)
+  end
 
 end
