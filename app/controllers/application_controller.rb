70dff7b0d8d1a39199f56e26e05718b646e5151e
Stephen <stephen@agilestyle.com>
2015-04-01 13:41:44 -0600

    Prevent registrations to site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :disable_sign_up, :if => :devise_controller?
+  def disable_sign_up
+    if "#{params[:controller]}_#{params[:action]}".gsub('devise/', '') == 'registrations_new'
+      redirect_to root_path
+    end
+  end
 end
