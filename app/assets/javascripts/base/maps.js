cd22b1458a86735075506756dce7d274792dbb6e
Stephen <stephen@agilestyle.com>
2015-06-23 19:54:48 -0600

    Add accommodation info and map for west edmonton hotel.

--- app/assets/javascripts/base/maps.js
+++ app/assets/javascripts/base/maps.js
@@ -1,6 +1,6 @@
 (function($){
 
-  
+
   function venue_init() {
       // Basic options for a simple Google Map
       // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
@@ -11,12 +11,12 @@
           // The latitude and longitude to center the map (always required)
           center: new google.maps.LatLng(53.655865, -113.509439), // Hunter's Green Golf Club
 
-          // How you would like to style the map. 
+          // How you would like to style the map.
           // This is where you would paste any style found on Snazzy Maps.
           styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
       };
 
-      // Get the HTML DOM element that will contain your map 
+      // Get the HTML DOM element that will contain your map
       // We are using a div with id="map" seen below in the <body>
       var mapElement = document.getElementById('venue-map');
 
@@ -58,17 +58,17 @@
       // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
       var mapOptions = {
           // How zoomed in you want the map to start at (always required)
-          zoom: 13,
+          zoom: 12,
 
           // The latitude and longitude to center the map (always required)
-          center: new google.maps.LatLng(53.582005, -113.56024), // Hunter's Green Golf Club
+          center: new google.maps.LatLng(53.5531783, -113.5808017), // Hunter's Green Golf Club
 
-          // How you would like to style the map. 
+          // How you would like to style the map.
           // This is where you would paste any style found on Snazzy Maps.
           styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
       };
 
-      // Get the HTML DOM element that will contain your map 
+      // Get the HTML DOM element that will contain your map
       // We are using a div with id="map" seen below in the <body>
       var mapElement = document.getElementById('accommodation-map');
 
@@ -76,13 +76,13 @@
       var map = new google.maps.Map(mapElement, mapOptions);
 
       // Let's also add a marker while we're at it
-      var marker = new google.maps.Marker({
+      var markerNorth = new google.maps.Marker({
           position: new google.maps.LatLng(53.582005, -113.56024),
           map: map,
           title: "Chateau Nova Yellowhead"
       });
 
-      var contentString = '<div class="venue-map-tooltip">'+
+      var contentStringNorth = '<div class="venue-map-tooltip">'+
         '<h3 class="map-heading">Chateau Nova Yellowhead</h3>'+
         '<p>'+
         '13920 Yellowhead Trail'+
@@ -94,12 +94,41 @@
         '</a>'+
         '</div>';
 
-      var infowindow = new google.maps.InfoWindow({
-        content: contentString
+      var infowindowNorth = new google.maps.InfoWindow({
+        content: contentStringNorth
       });
 
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map,marker);
+      google.maps.event.addListener(markerNorth, 'click', function() {
+        infowindowNorth.open(map,markerNorth);
+      });
+
+
+
+      // Let's also add a marker while we're at it
+      var markerWest = new google.maps.Marker({
+          position: new google.maps.LatLng(53.538344, -113.628309),
+          map: map,
+          title: "Executive Royal Inn"
+      });
+
+      var contentStringWest = '<div class="venue-map-tooltip">'+
+        '<h3 class="map-heading">Executive Royal Inn</h3>'+
+        '<p>'+
+        '10010 178 Street'+
+        '<br>'+
+        'Edmonton, AB T5S 1T3'+
+        '</p>'+
+        '<a target="_blank" href="https://goo.gl/maps/J0Hol">'+
+        'View on Google Maps'+
+        '</a>'+
+        '</div>';
+
+      var infowindowWest = new google.maps.InfoWindow({
+        content: contentStringWest
+      });
+
+      google.maps.event.addListener(markerWest, 'click', function() {
+        infowindowWest.open(map,markerWest);
       });
   }
   // When the window has finished loading create our google map below
